44abab22dcd025f2554c39d3c9f87971ace4dd6e
Jose Augusto Costa <75041668+JoseAugustoDev@users.noreply.github.com>
2023-11-27 21:43:35 -0300

    Main page and Login

--- python/app.py
+++ python/app.py
@@ -3,5 +3,13 @@ from Flask import Flask, redirect, render_template, request, url_for
 app = Flask(__name__)
 
 @app.route('/')
+def index_page():
+    return render_template('/index.html')
+
+@app.route('/login')
 def login_page():
-    return render_template('html/login.html')
+    return render_template('/html/login.html')
+
+@app.route('/main')
+def main_page():
+    return render_template('/html/main.html')
